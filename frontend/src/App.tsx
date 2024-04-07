d64294c6c2c06c5692867271890297ac152fd169
thakurxbhushan <153585277+thakurxbhushan@users.noreply.github.com>
2024-04-08 01:35:39 +0530

    Add files via upload

---
+++ frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from '@/pages/home-page';
+import AddBlog from '@/pages/add-blog';
+import DetailsPage from '@/pages/details-page';
+import ScrollToTop from '@/components/scroll-to-top';
+import Footer from '@/layouts/footer-layout';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <ScrollToTop />
+      <div className="flex min-h-screen flex-col">
+        <Routes>
+          <Route path="/">
+            <Route index element={<HomePage />} />
+            <Route path="add-blog" element={<AddBlog />} />
+            <Route path="details-page/:title/:postId" element={<DetailsPage />} />
+          </Route>
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
